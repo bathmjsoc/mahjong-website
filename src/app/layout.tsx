a955f7a880a80b620efe5f5d0d84f4b6af718bcd
QuantumXenon <ea994@bath.ac.uk>
2025-12-21 15:57:52 +0100

    fix: Add global font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
-import "./globals.css";
+import { Montserrat } from "next/font/google";
+import "./styles.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "500",
+});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body>{children}</body>
     </html>
   );
