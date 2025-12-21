66877b86df7158c73e74e05b2ce27d27d25c375c
QuantumXenon <ea994@bath.ac.uk>
2025-12-21 15:57:41 +0100

    fix: Add page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-export default function Home() {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default async function Home() {
   return (
-    <main className="text-blue-600">
+    <main className="flex flex-col min-h-screen justify-center items-center">
       <p>Hello World!</p>
     </main>
   );
