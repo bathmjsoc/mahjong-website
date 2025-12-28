a433d8a4ae8e19475ad82a029e4c2c8c888004b3
QuantumXenon <ea994@bath.ac.uk>
2025-12-28 15:31:33 +0100

    feat: Add fill/shuffle button placeholders

--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -1,4 +1,6 @@
+import { NoSymbolIcon } from "@heroicons/react/24/solid";
 import TableCard from "@/components/TableCard";
+import IconButton from "@/elements/IconButton";
 import { sortTablesId } from "@/lib/tables";
 import type { Player, Table } from "@/lib/types";
 
@@ -11,7 +13,15 @@ export default function TableList({ tables, players }: TableListProps) {
   const sortedTables = sortTablesId(tables);
 
   return (
-    <div className="flex justify-center w-full h-min p-3">
+    <div className="flex flex-col items-center justify-center w-full h-min p-3">
+      <div className="flex space-x-10 my-7">
+        <IconButton className="bg-(--primary-color) rounded-full p-2">
+          <NoSymbolIcon className="size-8" />
+        </IconButton>
+        <IconButton className="bg-(--primary-color) rounded-full p-2">
+          <NoSymbolIcon className="size-8" />
+        </IconButton>
+      </div>
       <div className="grid grid-cols-[repeat(auto-fit,280px)] gap-10 w-full justify-center">
         {sortedTables.map((table) => (
           <TableCard key={table.id} table={table} players={players}></TableCard>
