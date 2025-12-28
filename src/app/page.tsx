5cf442c147c93b989632525b7fc0850d67c24703
QuantumXenon <ea994@bath.ac.uk>
2025-12-28 01:45:12 +0100

    fix: Correct missed refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import ColoredLink from "@/elements/ColoredLink";
+import TextLink from "@/elements/TextLink";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col min-h-dvh items-center justify-center">
       <span>Hello World!</span>
-      <ColoredLink href="/login">Login</ColoredLink>
+      <TextLink href="/login">Login</TextLink>
     </main>
   );
 }
