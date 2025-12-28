faa032d7d249c5096dc89540e3f620c7c6849f4a
QuantumXenon <ea994@bath.ac.uk>
2025-12-28 15:08:14 +0100

    refactor: Clean up UUID param usage

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -4,24 +4,23 @@ import { useParams } from "next/navigation";
 import { TabLink, TabMenu } from "@/elements/TabMenu";
 
 export default function Topbar() {
-  const params = useParams();
+  const { uuid } = useParams();
 
   return (
     <nav
       className="
-        bg-(--accent-color) text-(--secondary-color)
-        flex space-x-5 w-full items-center
-        h-15 pl-5 z-50 shadow-2xl
+        bg-(--accent-color)
+        flex items-center w-full h-15 pl-5 z-50 shadow-2xl
       "
     >
       <TabMenu>
-        <TabLink href={`/tournament/${params.uuid}`} className="w-30">
+        <TabLink href={`/tournament/${uuid}`} className="w-30">
           Tables
         </TabLink>
-        <TabLink href={`/tournament/${params.uuid}/logs`} className="w-30">
+        <TabLink href={`/tournament/${uuid}/logs`} className="w-30">
           Logs
         </TabLink>
-        <TabLink href={`/tournament/${params.uuid}/graphs`} className="w-30 ">
+        <TabLink href={`/tournament/${uuid}/graphs`} className="w-30 ">
           Graphs
         </TabLink>
       </TabMenu>
