6a66ee3693b863f23257db42f1d966c0fa30cfae
QuantumXenon <ea994@bath.ac.uk>
2026-02-20 16:40:04 +0000

    feat: Implement supabase auth

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,11 +1,31 @@
 "use server";
 
-export async function signUp(email: string, password: string): Promise<void> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log(`signIn(email=${email}, password=${password})`);
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase";
+
+export type ActionState = {
+  error?: string;
+  success?: boolean;
+} | null;
+
+export async function signUp(_: ActionState, formData: FormData) {
+  const supabase = await createClient();
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const { error } = await supabase.auth.signUp({ email, password });
+  if (error) return { error: error.message };
+
+  return { success: true };
 }
 
-export async function signIn(email: string, password: string): Promise<void> {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log(`signIn(email=${email}, password=${password})`);
+export async function signIn(_: ActionState, formData: FormData) {
+  const supabase = await createClient();
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) return { error: error.message };
+
+  redirect("/dashboard");
 }
