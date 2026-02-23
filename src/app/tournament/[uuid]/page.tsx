b24ff3683b68d9d0cb5ab7d8eadc0cb33e3ca88f
QuantumXenon <ea994@bath.ac.uk>
2026-02-23 16:31:20 +0000

    fix: Move shuffle button

--- src/app/tournament/[uuid]/page.tsx
+++ src/app/tournament/[uuid]/page.tsx
@@ -9,15 +9,15 @@ export default function TournamentPage() {
   return (
     <main className="flex min-h-dvh">
       <Sidebar />
-      <section className="flex flex-col items-center w-full h-min">
-        <div className="flex space-x-10 my-7">
-          <IconButton className="bg-(--primary-color) rounded-full p-3 hover:text-(--accent-color)">
-            <PaintBucket className="size-5" />
-          </IconButton>
-          <IconButton className="bg-(--primary-color) rounded-full p-3 hover:text-(--accent-color)">
-            <Shuffle className="size-5" />
-          </IconButton>
-        </div>
+      <section className="flex flex-col items-center w-full h-min pt-5 pr-10">
+        <IconButton
+          className="
+            bg-(--primary-color) rounded-l-full w-10 h-15 p-3
+            fixed right-0 top-1/2 -translate-y-1/2 -mr-1
+          "
+        >
+          <Shuffle className="size-5" />
+        </IconButton>
         <TableList />
       </section>
     </main>
