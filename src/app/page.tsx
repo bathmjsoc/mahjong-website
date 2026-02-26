82f178d2dca997abb4ae6fe92464fc7dddb013b5
edwinauton <ea994@bath.ac.uk>
2026-02-26 02:10:04 +0000

    feat: Add credits footer to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 import { LoginForm } from "@/components/LoginForm";
 
 export const metadata: Metadata = {
   title: "mahjong-website",
 };
 
-export default async function LoginPage() {
+export default function LoginPage() {
   return (
     <div className="flex min-h-dvh items-center justify-center">
       <LoginForm />
+
+      <footer className="fixed bottom-2 text-xs">
+        <span>
+          Developed by{" "}
+          <Link
+            href="https://github.com/edwinauton"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            Edwin Auton
+          </Link>
+        </span>
+        <span className="mx-2">/</span>
+        <span>
+          Original Concept by{" "}
+          <Link
+            href="https://github.com/duskt"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            Guy Johns
+          </Link>
+        </span>
+      </footer>
     </div>
   );
 }
