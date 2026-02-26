ff7e463e57031331e3988a16ce8bd295b8387cc4
QuantumXenon <ea994@bath.ac.uk>
2026-02-26 16:50:21 +0000

    fix: Update credit banner

--- src/components/CreditsBanner.tsx
+++ src/components/CreditsBanner.tsx
@@ -2,25 +2,29 @@ import Link from "next/link";
 
 export function CreditsBanner() {
   return (
-    <footer className="fixed bottom-2 text-primary text-xs">
-      <span>
-        Developed by{" "}
+    <footer
+      className="
+        bg-primary text-secondary
+        flex gap-2 px-4 py-2 fixed bottom-0 left-0
+        rounded-tr-xl text-[10px] uppercase
+      "
+    >
+      <span className="flex gap-1.5">
+        Built by
         <Link
           href="https://github.com/edwinauton"
           target="_blank"
-          rel="noopener noreferrer"
           className="hover:underline"
         >
           Edwin Auton
         </Link>
       </span>
-      <span className="mx-2">/</span>
-      <span>
-        Original Concept by{" "}
+      /
+      <span className="flex gap-1.5">
+        Concept by
         <Link
           href="https://github.com/duskt"
           target="_blank"
-          rel="noopener noreferrer"
           className="hover:underline"
         >
           Guy Johns
