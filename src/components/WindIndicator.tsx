1a1107764ab7181e4d4e430e8659efaf85950040
QuantumXenon <ea994@bath.ac.uk>
2026-02-26 15:10:48 +0000

    refactor: Clean up WindIndicator

--- src/components/WindIndicator.tsx
+++ src/components/WindIndicator.tsx
@@ -9,7 +9,7 @@ export function WindIndicator() {
   const [wind, setWind] = useState<string | null>(WINDS[0]);
 
   return (
-    <div className="absolute top-20 right-5">
+    <div className="bg-primary absolute top-20 right-5 rounded-2xl">
       <RoundedListbox<string>
         value={wind}
         options={WINDS}
@@ -17,7 +17,6 @@ export function WindIndicator() {
         getOptionLabel={(wind) => wind}
         getOptionKey={(wind) => wind}
         buttonClassName="border-primary border-2 size-20 text-5xl font-normal rounded-2xl"
-        optionsClassName="w-auto"
       />
     </div>
   );
