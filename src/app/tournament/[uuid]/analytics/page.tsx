2eef109a4d6e295f7d8eb169adfb89e449a3a574
Edwin <ea994@bath.ac.uk>
2026-06-28 19:48:53 +0900

    feat: Sample graph on analytics page

--- src/app/tournament/[uuid]/analytics/page.tsx
+++ src/app/tournament/[uuid]/analytics/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import {LineGraph, SeriesData} from "@/components/LineGraph";
+
+const data: SeriesData[] = [
+  {
+    name: "Player A",
+    points: [
+      { "x": "Session A", "y": -42 },
+      { "x": "Session B", "y": 87 },
+      { "x": "Session C", "y": -12 },
+      { "x": "Session D", "y": 55 },
+      { "x": "Session E", "y": -93 },
+      { "x": "Session F", "y": 21 },
+      { "x": "Session G", "y": -68 },
+      { "x": "Session H", "y": 4 },
+      { "x": "Session I", "y": -77 },
+      { "x": "Session J", "y": 33 }
+    ]
+  }
+];
+
 export default function AnalyticsPage() {
-  return <p>TODO: Implement the analytics page</p>;
+  return (
+    <div className="flex flex-col items-center gap-10 p-10 w-3xl">
+      <LineGraph data={data}  />
+    </div>
+  );
 }
