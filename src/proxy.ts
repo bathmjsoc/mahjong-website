489d79b993854b70f827df5c5bb7b95184aab9f5
Edwin <ea994@bath.ac.uk>
2026-07-08 18:49:57 +0900

    refactor: Use list of public routes for guarding

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,21 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
+const publicRoutes = [
+  "/"
+]
+
 export async function proxy(request: NextRequest) {
   const { user, response } = await updateSession(request);
   const  pathname = request.nextUrl.pathname;
 
   // Route unauthenticated users to login page
-  if (!user && pathname !== "/") {
+  if (!user && !publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
   // Route authenticated users away from login page
-  if (user && pathname === "/") {
+  if (user && publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
