6544fda283425e08f2b5470324b62da807b07eee
Edwin <ea994@bath.ac.uk>
2026-07-10 22:43:23 +0900

    fix: Use correct `queryKey`

--- src/hooks/useAttendanceRealtime.ts
+++ src/hooks/useAttendanceRealtime.ts
@@ -25,7 +25,7 @@ export function useAttendanceRealtime() {
         },
         () =>
           queryClient.invalidateQueries({
-            queryKey: ["sessions", currentSession.id],
+            queryKey: ["attendance", currentSession.id],
           }),
       )
       .subscribe();
