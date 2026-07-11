5dcd61af6e896695311ab44e31fabcb6a0a01bec
Edwin <ea994@bath.ac.uk>
2026-07-11 21:55:42 +0900

    feat: Add table to set scoring rules for a tournament

---
+++ src/components/ScoringRulesTable.tsx
@@ -0,0 +1,126 @@
+import type { Dispatch, SetStateAction } from "react";
+import { LabelledInput } from "@/elements/LabelledInput";
+import type { ScoringRule, WinType } from "@/lib/types";
+
+const WIN_TYPES: WinType[] = ["打出", "自摸", "包自摸"] as const;
+
+type ScoringRulesTableProps = {
+  scoringRules: ScoringRule[];
+  setScoringRules: Dispatch<SetStateAction<ScoringRule[]>>;
+};
+
+export function ScoringRulesTable({
+  scoringRules,
+  setScoringRules,
+}: ScoringRulesTableProps) {
+  function handleRuleChange(index: number, newRule: ScoringRule) {
+    setScoringRules((prevRule) => prevRule.with(index, newRule));
+  }
+
+  return (
+    <table className="w-full table-fixed border-collapse">
+      <thead className="sticky top-0 z-10 bg-primary">
+        <tr>
+          <th rowSpan={2}>Faan</th>
+
+          {WIN_TYPES.map((winType) => (
+            <th key={winType} colSpan={2} className="border-secondary border-l">
+              {winType}
+            </th>
+          ))}
+        </tr>
+
+        <tr>
+          {WIN_TYPES.flatMap((winType) => [
+            <th
+              key={`${winType}_winner`}
+              className="border-secondary border-l text-xs"
+            >
+              Winner
+            </th>,
+
+            <th key={`${winType}_loser`} className="text-xs">
+              Loser
+            </th>,
+          ])}
+        </tr>
+      </thead>
+
+      <tbody>
+        {scoringRules.map((rule, index) => (
+          <ScoringRuleRow
+            key={index}
+            rule={rule}
+            onChange={(updatedRule) => handleRuleChange(index, updatedRule)}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+type ScoringRuleRowProps = {
+  rule: ScoringRule;
+  onChange: (updatedRule: ScoringRule) => void;
+};
+
+function ScoringRuleRow({ rule, onChange }: ScoringRuleRowProps) {
+  function updateFaan(value: number) {
+    const nextRule = structuredClone(rule);
+    nextRule.faan = value;
+    onChange(nextRule);
+  }
+
+  function updateDelta(
+    winType: WinType,
+    field: "winner" | "loser",
+    value: number,
+  ) {
+    const nextRule = structuredClone(rule);
+
+    if (nextRule.deltas[winType]) {
+      nextRule.deltas[winType][field] = value;
+      onChange(nextRule);
+    }
+  }
+
+  return (
+    <tr>
+      <td className="px-2 py-1">
+        <LabelledInput
+          type="number"
+          defaultValue={rule.faan}
+          inputClassName="no-spinner"
+          onBlur={(e) => updateFaan(Number(e.target.value))}
+        />
+      </td>
+
+      {WIN_TYPES.flatMap((winType) => [
+        <td
+          key={`${winType}_winner_delta`}
+          className="border-secondary border-l px-2 py-1"
+        >
+          <LabelledInput
+            type="number"
+            defaultValue={rule.deltas[winType]?.winner}
+            inputClassName="no-spinner"
+            onBlur={(e) =>
+              updateDelta(winType, "winner", Number(e.target.value))
+            }
+          />
+        </td>,
+
+        <td key={`${winType}_loser_delta`} className="px-2 py-1">
+          <LabelledInput
+            type="number"
+            defaultValue={rule.deltas[winType]?.loser}
+            inputClassName="no-spinner"
+            onBlur={(e) =>
+              updateDelta(winType, "loser", Number(e.target.value))
+            }
+          />
+        </td>,
+      ])}
+    </tr>
+  );
+}
--- src/components/modals/CreateTournamentModal.tsx
+++ src/components/modals/CreateTournamentModal.tsx
@@ -1,7 +1,20 @@
 import { useQueryClient } from "@tanstack/react-query";
+import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 import { createTournament } from "@/actions/tournaments";
+import { ScoringRulesTable } from "@/components/ScoringRulesTable";
+import { FilledButton } from "@/elements/FilledButton";
 import { LabelledInput } from "@/elements/LabelledInput";
 import { Modal } from "@/elements/Modal";
+import type { PointDelta, ScoringRule } from "@/lib/types";
+
+function createEmptyDeltas(): Record<string, PointDelta> {
+  return {
+    打出: { winner: 0, loser: 0 },
+    自摸: { winner: 0, loser: 0 },
+    包自摸: { winner: 0, loser: 0 },
+  };
+}
 
 type CreateTournamentModalProps = {
   isOpen: boolean;
@@ -14,9 +27,15 @@ export function CreateTournamentModal({
 }: CreateTournamentModalProps) {
   const queryClient = useQueryClient();
 
+  const [scoringRules, setScoringRules] = useState<ScoringRule[]>([
+    { faan: 0, deltas: createEmptyDeltas() },
+  ]);
+
   async function handleSubmit(formData: FormData) {
     const tournamentName = formData.get("tournamentName")?.toString();
     if (!tournamentName) return;
+
+    console.log(scoringRules);
 
     await createTournament(tournamentName);
     await queryClient.invalidateQueries({ queryKey: ["tournaments"] });
@@ -24,18 +43,66 @@ export function CreateTournamentModal({
     closeModalAction();
   }
 
+  function handleAddRule() {
+    const newRule: ScoringRule = {
+      faan: 0,
+      deltas: {
+        打出: { winner: 0, loser: 0 },
+        自摸: { winner: 0, loser: 0 },
+        包自摸: { winner: 0, loser: 0 },
+      },
+    };
+
+    setScoringRules((rules) => [...rules, newRule]);
+  }
+
+  function handleRemoveRule() {
+    setScoringRules((rules) => rules.slice(0, -1));
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={closeModalAction} title="Create Tournament">
-      <form action={handleSubmit} className="flex w-xs flex-col gap-3">
+      <form
+        action={handleSubmit}
+        className="flex h-120 max-h-120 w-3xl flex-col items-center justify-center gap-4"
+      >
         <LabelledInput
           name="tournamentName"
           type="text"
           autoComplete="off"
           autoFocus
           required
+          inputClassName="w-sm"
         >
           Tournament Name
         </LabelledInput>
+
+        <div className="scrollbar-thin scrollbar-thumb-secondary scrollbar-track-transparent flex h-full flex-col gap-4 overflow-y-auto">
+          <ScoringRulesTable
+            scoringRules={scoringRules}
+            setScoringRules={setScoringRules}
+          />
+
+          <div className="flex items-center justify-center gap-4">
+            <FilledButton
+              onClick={handleAddRule}
+              className="size-9 rounded-full bg-positive"
+            >
+              <Plus className="size-5" />
+            </FilledButton>
+
+            <FilledButton
+              onClick={handleRemoveRule}
+              className="size-9 rounded-full bg-negative"
+            >
+              <Minus className="size-5" />
+            </FilledButton>
+          </div>
+        </div>
+
+        <FilledButton type="submit" className="w-sm bg-accent">
+          Create Tournament
+        </FilledButton>
       </form>
     </Modal>
   );
