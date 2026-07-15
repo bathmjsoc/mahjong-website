e1a9b8fde87581ccf1e8aee4f2cad09cd1701efe
Edwin <ea994@bath.ac.uk>
2026-07-15 23:33:23 +0900

    feat: Modal to show session bar chart

---
+++ src/components/modals/ViewGraphModal.tsx
@@ -0,0 +1,33 @@
+import { BarChart } from "@/elements/BarChart";
+import { Modal } from "@/elements/Modal";
+import type { Player } from "@/lib/types";
+import { rankPlayers } from "@/lib/utils";
+
+type ViewGraphModalProps = {
+  players: Player[];
+  scores: Record<string, number>;
+  isOpen: boolean;
+  closeModalAction: () => void;
+};
+
+export function ViewGraphModal({
+  players,
+  scores,
+  isOpen,
+  closeModalAction,
+}: ViewGraphModalProps) {
+  const rankedPlayers = rankPlayers(players, scores);
+
+  const data = rankedPlayers.map(([player, score]) => ({
+    label: player.name,
+    value: score,
+  }));
+
+  return (
+    <Modal isOpen={isOpen} onClose={closeModalAction} title="View Graph">
+      <div className="w-xl p-2">
+        <BarChart data={data} className="rounded-xl"/>
+      </div>
+    </Modal>
+  );
+}
