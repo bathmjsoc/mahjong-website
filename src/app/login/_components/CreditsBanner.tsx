f9ae3adec311ff7f32ab8afec3dae34e61d7a839
Edwin <ea994@bath.ac.uk>
2026-07-25 13:49:12 +0900

    refactor: Split `CreditsBanner` into separate file

---
+++ src/app/login/_components/CreditsBanner.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+export function CreditsBanner() {
+  return (
+    <footer className="fixed bottom-0 left-0 flex gap-2 rounded-tr-xl bg-primary px-4 py-2 text-[10px] text-secondary uppercase">
+      <span className="flex gap-1.5">
+        Website by
+        <Link
+          href="https://github.com/edwinauton"
+          target="_blank"
+          className="hover:underline"
+        >
+          Edwin Auton
+        </Link>
+      </span>
+      /
+      <span className="flex gap-1.5">
+        Original by
+        <Link
+          href="https://github.com/duskt"
+          target="_blank"
+          className="hover:underline"
+        >
+          Guy Johns
+        </Link>
+      </span>
+    </footer>
+  );
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { CreditsBanner } from "./_components/CreditsBanner";
 import { LoginForm } from "./_components/LoginForm";
 
 export default function LoginPage() {
@@ -7,33 +7,5 @@ export default function LoginPage() {
       <LoginForm />
       <CreditsBanner />
     </div>
-  );
-}
-
-function CreditsBanner() {
-  return (
-    <footer className="fixed bottom-0 left-0 flex gap-2 rounded-tr-xl bg-primary px-4 py-2 text-[10px] text-secondary uppercase">
-      <span className="flex gap-1.5">
-        Website by
-        <Link
-          href="https://github.com/edwinauton"
-          target="_blank"
-          className="hover:underline"
-        >
-          Edwin Auton
-        </Link>
-      </span>
-      /
-      <span className="flex gap-1.5">
-        Original by
-        <Link
-          href="https://github.com/duskt"
-          target="_blank"
-          className="hover:underline"
-        >
-          Guy Johns
-        </Link>
-      </span>
-    </footer>
   );
 }
