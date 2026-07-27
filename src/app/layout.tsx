ff558f769e419dfa036644fda3c4aef94fcf2e2f
Edwin <ea994@bath.ac.uk>
2026-07-28 02:50:34 +0900

    refactor: Move suspense loader to separate file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Oxygen_Mono } from "next/font/google";
-import { type PropsWithChildren, Suspense } from "react";
+import type { PropsWithChildren } from "react";
 import { QueryProvider } from "@/providers/QueryProvider";
 import "./globals.css";
 
@@ -17,9 +17,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={oxygenMono.className}>
-        <QueryProvider>
-          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
-        </QueryProvider>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
