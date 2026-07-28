5657bb8a6ef0f39992c253f6cb189bbdc19828ab
Edwin <ea994@bath.ac.uk>
2026-07-29 01:00:15 +0900

    refactor: Reorganise `useTournaments`

--- src/hooks/tournaments/useTournaments.ts
+++ src/hooks/tournaments/useTournaments.ts
@@ -1,61 +1,40 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useCallback } from "react";
-import type { Tables } from "@/lib/database.types";
 import { createClient } from "@/lib/supabase/client";
-import type { ScoringRule, Tournament } from "@/lib/types";
+import type { ScoringRule, SupabaseTournament, Tournament } from "@/lib/types";
 import { useTournamentContext } from "@/providers/TournamentProvider";
 
 type UseTournamentsType = {
-  currentTournament: Tournament | null;
-  scoringRules: ScoringRule[];
+  scoring_rules: ScoringRule[];
 };
 
 export function useTournaments(): UseTournamentsType {
   const { tournamentId } = useTournamentContext();
 
-  const selectTournaments = useCallback(
-    (rawTournaments: Tournament[]) => {
-      const tournaments = [...rawTournaments];
-
-      const currentTournament =
-        tournaments.find((tournament) => tournament.id === tournamentId) ??
-        null;
-
-      const scoringRules = currentTournament?.scoring_rules ?? [];
-
-      return { currentTournament, scoringRules };
-    },
-    [tournamentId],
-  );
-
   const query = useSuspenseQuery({
-    queryKey: ["tournaments"],
-    queryFn: fetchTournaments,
-    select: selectTournaments,
+    queryKey: ["tournament", tournamentId],
+    queryFn: () => fetchTournamentById(tournamentId),
   });
 
-  return {
-    currentTournament: query.data.currentTournament,
-    scoringRules: query.data.scoringRules,
-  };
+  return query.data;
 }
 
-// Supabase stores `scoring_rules` as jsonb so we need to override it with the correct type
-type TournamentRow = Omit<Tables<"tournaments">, "scoring_rules"> & {
-  scoring_rules: ScoringRule[];
-};
-
-export async function fetchTournaments(): Promise<Tournament[]> {
+export async function fetchTournamentById(
+  tournamentId: string,
+): Promise<Tournament> {
   const supabase = createClient();
 
-  const { data: tournaments, error } = await supabase
+  const { data: tournament, error } = await supabase
     .from("tournaments")
     .select("*")
-    .order("last_updated", { ascending: false })
-    .overrideTypes<TournamentRow[]>(); // Not ideal, but otherwise the typing breaks :(
+    .eq("id", tournamentId)
+    .single()
+    .overrideTypes<SupabaseTournament>();
 
-  if (error)
-    throw new Error(`fetchTournaments encountered an error: ${error.message}`);
+  if (error) {
+    throw new Error(
+      `fetchTournamentById encountered an error: ${error.message}`,
+    );
+  }
 
-  return tournaments ?? [];
+  return tournament;
 }
