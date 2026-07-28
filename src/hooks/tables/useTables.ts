008f5c58e1f5742d6e96eedb3d701636e5fb7245
Edwin <ea994@bath.ac.uk>
2026-07-29 00:57:37 +0900

    refactor: Reorganise `useTables`

--- src/hooks/tables/useTables.ts
+++ src/hooks/tables/useTables.ts
@@ -1,8 +1,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useCallback } from "react";
-import { useSessions } from "@/hooks/sessions/useSessions";
 import { createClient } from "@/lib/supabase/client";
-import type { Session, Table } from "@/lib/types";
+import type { Table } from "@/lib/types";
+import { useSessionContext } from "@/providers/SessionProvider";
 
 type UseTablesType = {
   availableTables: Table[];
@@ -12,78 +11,70 @@ type UseTablesType = {
 };
 
 export function useTables(): UseTablesType {
-  const { currentSession } = useSessions();
-
-  const selectTables = useCallback((rawTables: Table[]) => {
-    const tables = [...rawTables].sort((a, b) => {
-      if (a.saved !== b.saved) {
-        return Number(a.saved) - Number(b.saved);
-      }
-
-      return a.number - b.number;
-    });
-
-    const availableTables = [];
-    const duplicatePlayerIds = new Set<string>();
-    const seatedPlayerIds = new Set<string>();
-
-    for (const table of tables) {
-      if (table.saved) continue;
-      availableTables.push(table);
-
-      const SEAT_IDS = [
-        table.east_id,
-        table.south_id,
-        table.west_id,
-        table.north_id,
-      ] as const;
-
-      for (const id of SEAT_IDS) {
-        if (!id) continue;
-
-        if (seatedPlayerIds.has(id)) {
-          duplicatePlayerIds.add(id);
-        } else {
-          seatedPlayerIds.add(id);
-        }
-      }
-    }
-
-    return {
-      tables,
-      availableTables,
-      duplicatePlayerIds,
-      seatedPlayerIds,
-    };
-  }, []);
+  const { sessionId } = useSessionContext();
 
   const query = useSuspenseQuery({
-    queryKey: ["tables", currentSession?.id],
-    queryFn: () => {
-      if (!currentSession) return [];
-      return fetchTables(currentSession);
-    },
+    queryKey: ["tables", sessionId],
+    queryFn: () => fetchTables(sessionId),
     select: selectTables,
   });
 
+  return query.data;
+}
+
+function selectTables(rawTables: Table[]): UseTablesType {
+  const tables = [...rawTables].sort((a, b) => {
+    if (a.saved !== b.saved) {
+      return Number(a.saved) - Number(b.saved);
+    }
+    return a.number - b.number;
+  });
+
+  const availableTables = [];
+  const duplicatePlayerIds = new Set<string>();
+  const seatedPlayerIds = new Set<string>();
+
+  for (const table of tables) {
+    if (table.saved) continue;
+    availableTables.push(table);
+
+    const seatIds = [
+      table.east_id,
+      table.south_id,
+      table.west_id,
+      table.north_id,
+    ] as const;
+
+    for (const id of seatIds) {
+      if (!id) continue;
+
+      if (seatedPlayerIds.has(id)) {
+        duplicatePlayerIds.add(id);
+      } else {
+        seatedPlayerIds.add(id);
+      }
+    }
+  }
+
   return {
-    availableTables: query.data.availableTables,
-    duplicatePlayerIds: query.data.duplicatePlayerIds,
-    seatedPlayerIds: query.data.seatedPlayerIds,
-    tables: query.data.tables,
+    tables,
+    availableTables,
+    duplicatePlayerIds,
+    seatedPlayerIds,
   };
 }
 
-async function fetchTables(session: Session): Promise<Table[]> {
+async function fetchTables(sessionId: string): Promise<Table[]> {
   const supabase = createClient();
 
   const { data: tables, error } = await supabase
     .from("tables")
     .select("*")
-    .eq("session_id", session.id);
+    .eq("session_id", sessionId);
 
-  if (error)
+  if (error) {
     throw new Error(`fetchTables encountered an error: ${error.message}`);
+  }
 
   return tables ?? [];
 }
