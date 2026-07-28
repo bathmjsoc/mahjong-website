3362cc64031103aebd6023cc300e7596af307ef9
Edwin <ea994@bath.ac.uk>
2026-07-29 00:58:52 +0900

    refactor: Reorganise `useSessions`

--- src/hooks/sessions/useSessions.ts
+++ src/hooks/sessions/useSessions.ts
@@ -1,56 +1,45 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useCallback } from "react";
-import { useTournaments } from "@/hooks/tournaments/useTournaments";
 import { createClient } from "@/lib/supabase/client";
-import type { Session, Tournament } from "@/lib/types";
+import type { Session } from "@/lib/types";
+import { useTournamentContext } from "@/providers/TournamentProvider";
 
 type UseSessionsType = {
-  currentSession: Session | null;
   sessionMap: Record<string, Session>;
   sessions: Session[];
 };
 
 export function useSessions(): UseSessionsType {
-  const { currentTournament } = useTournaments();
-
-  const selectSessions = useCallback((rawSessions: Session[]) => {
-    const sessions = [...rawSessions].sort(
-      (a, b) =>
-        new Date(a.start_date).getTime() - new Date(b.start_date).getTime(),
-    );
-
-    const currentSession = sessions.at(-1) ?? null;
-
-    const sessionMap = Object.fromEntries(
-      sessions.map((session) => [session.id, session]),
-    );
-
-    return { sessions, currentSession, sessionMap };
-  }, []);
+  const { tournamentId } = useTournamentContext();
 
   const query = useSuspenseQuery({
-    queryKey: ["sessions", currentTournament?.id],
-    queryFn: () => {
-      if (!currentTournament) return [];
-      return fetchSessions(currentTournament);
-    },
+    queryKey: ["sessions", tournamentId],
+    queryFn: () => fetchSessions(tournamentId),
     select: selectSessions,
   });
 
-  return {
-    currentSession: query.data.currentSession,
-    sessionMap: query.data.sessionMap,
-    sessions: query.data.sessions,
-  };
+  return query.data;
 }
 
-async function fetchSessions(tournament: Tournament): Promise<Session[]> {
+function selectSessions(rawSessions: Session[]): UseSessionsType {
+  const sessions = [...rawSessions].sort(
+    (a, b) =>
+      new Date(a.start_date).getTime() - new Date(b.start_date).getTime(),
+  );
+
+  const sessionMap = Object.fromEntries(
+    sessions.map((session) => [session.id, session]),
+  );
+
+  return { sessions, sessionMap };
+}
+
+async function fetchSessions(tournamentId: string): Promise<Session[]> {
   const supabase = createClient();
 
   const { data: sessions, error } = await supabase
     .from("sessions")
     .select("*")
-    .eq("tournament_id", tournament.id);
+    .eq("tournament_id", tournamentId);
 
   if (error)
     throw new Error(`fetchSessions encountered an error: ${error.message}`);
