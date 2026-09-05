f4d03d80283c400d7d3844818f97c18bfdd70223
Edwin <ea994@bath.ac.uk>
2026-09-05 19:30:14 +0900

    refactor: Adjust page layout

--- src/app/tournament/[uuid]/analytics/page.tsx
+++ src/app/tournament/[uuid]/analytics/page.tsx
@@ -4,8 +4,11 @@ import { useState } from "react";
 import { RoundedListbox } from "@/elements/RoundedListbox";
 import { usePlayers } from "@/hooks/players/usePlayers";
 import type { Player } from "@/lib/types";
-import { GameResultsCard } from "./_components/GameResultsCard";
+import { FaanFrequencyCard } from "./_components/FaanFrequencyCard";
+import { GameOutcomesCard } from "./_components/GameOutcomesCard";
+import { RankingCard } from "./_components/RankingCard";
 import { ScoreHistoryCard } from "./_components/ScoreHistoryCard";
+import { StatisticsCard } from "./_components/StatisticsCard";
 
 export default function AnalyticsPage() {
   const { players } = usePlayers();
@@ -25,9 +28,27 @@ export default function AnalyticsPage() {
       />
 
       {selectedPlayer && (
-        <div className="flex items-start gap-5">
-          <ScoreHistoryCard player={selectedPlayer} />
-          <GameResultsCard player={selectedPlayer} />
+        <div className="flex items-center gap-5">
+          <div className="flex flex-col items-center gap-5">
+            <div className="flex items-center gap-5">
+              <div className="rounded-lg bg-primary p-5">
+                <RankingCard player={selectedPlayer} />
+              </div>
+
+              <div className="rounded-lg bg-primary p-5">
+                <GameOutcomesCard player={selectedPlayer} />
+              </div>
+            </div>
+
+            <div className="rounded-lg bg-primary p-5">
+              <StatisticsCard player={selectedPlayer} />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 rounded-lg bg-primary p-5">
+            <ScoreHistoryCard player={selectedPlayer} />
+            <FaanFrequencyCard player={selectedPlayer} />
+          </div>
         </div>
       )}
     </div>
