59722ccec3485ec4bd435667814382ad38e625e3
Edwin <ea994@bath.ac.uk>
2026-09-09 21:22:19 +0900

    fix: Adjust JSON payload data

--- src/app/tournament/[uuid]/sessions/page.tsx
+++ src/app/tournament/[uuid]/sessions/page.tsx
@@ -32,8 +32,9 @@ export default function SessionsPage() {
 
   function handleDownloadJSON() {
     const data = {
-      players: players,
       logs: logs,
+      players: players,
+      sessions: sessions,
     };
 
     const blob = new Blob([JSON.stringify(data, null, 2)], {
