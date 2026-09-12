f27ec507a1e981277153ecc7142eae8b09c36b1b
Edwin <ea994@bath.ac.uk>
2026-09-13 00:22:52 +0900

    refactor: Make `attendance` consistent with other actions/hooks

--- src/actions/attendance.ts
+++ src/actions/attendance.ts
@@ -1,40 +1,15 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
-import type { Attendance, Player } from "@/lib/types";
+import type { Attendance } from "@/lib/types";
 
-export async function registerPlayer(
-  sessionId: string,
-  player: Player,
-): Promise<void> {
-  const supabase = await createClient();
-  const { error } = await supabase.from("attendance").upsert(
-    {
-      session_id: sessionId,
-      player_id: player.id,
-      registered: true,
-      locked: false,
-    },
-    { onConflict: "session_id, player_id" },
-  );
-
-  if (error) {
-    throw new Error(`registerPlayer encountered an error: ${error.message}`);
-  }
-}
-
-export async function updateAttendance(
-  sessionId: string,
-  player: Player,
-  attendance: Partial<Attendance>,
-): Promise<void> {
+export async function upsertAttendance(attendance: Attendance): Promise<void> {
   const supabase = await createClient();
   const { error } = await supabase
     .from("attendance")
-    .update(attendance)
-    .match({ session_id: sessionId, player_id: player.id });
+    .upsert(attendance, { onConflict: "session_id, player_id" });
 
   if (error) {
-    throw new Error(`updateAttendance encountered an error: ${error.message}`);
+    throw new Error(`upsertAttendance encountered an error: ${error.message}`);
   }
 }
--- src/hooks/attendance/useAttendanceMutations.ts
+++ src/hooks/attendance/useAttendanceMutations.ts
@@ -1,90 +1,66 @@
-import {
-  registerPlayer as registerPlayerAction,
-  updateAttendance as updateAttendanceAction,
-} from "@/actions/attendance";
+import { upsertAttendance as upsertAttendanceAction } from "@/actions/attendance";
 import {
   useCacheItems,
   useOptimisticMutation,
 } from "@/hooks/useOptimisticUpdates";
 import type { Attendance, Player } from "@/lib/types";
 
-type RegisterAttendanceType = {
-  sessionId: string;
-  player: Player;
-};
-
-type UpdateAttendanceType = {
-  sessionId: string;
-  player: Player;
-  changes: Partial<Attendance>;
-};
-
 export function useAttendanceMutations() {
-  const getAttendanceQueryKey = (sessionId: string) => [
-    "attendance",
-    sessionId,
-  ];
+  const getAttendanceQueryKey = (attendance: Attendance) => {
+    return ["attendance", attendance.session_id];
+  };
 
-  const { addItem, patchItem } = useCacheItems<Attendance>({
-    getId: ({ session_id, player_id }) => `${session_id}:${player_id}`,
-    getQueryKey: (attendance) => getAttendanceQueryKey(attendance.session_id),
+  const { addItem, updateItem } = useCacheItems<Attendance>({
+    getId: (attendance) => `${attendance.session_id}:${attendance.player_id}`,
+    getQueryKey: getAttendanceQueryKey,
   });
 
-  const registerMutation = useOptimisticMutation<RegisterAttendanceType, void>({
-    mutationFn: ({ sessionId, player }) =>
-      registerPlayerAction(sessionId, player),
-    getQueryKey: ({ sessionId }) => getAttendanceQueryKey(sessionId),
-    optimisticUpdate: ({ sessionId, player }) =>
-      addItem({
-        session_id: sessionId,
-        player_id: player.id,
-        registered: true,
-        locked: false,
-      }),
+  const registerMutation = useOptimisticMutation({
+    mutationFn: upsertAttendanceAction,
+    getQueryKey: getAttendanceQueryKey,
+    optimisticUpdate: addItem,
   });
 
-  const updateMutation = useOptimisticMutation<UpdateAttendanceType, void>({
-    mutationFn: ({ sessionId, player, changes }) =>
-      updateAttendanceAction(sessionId, player, changes),
-    getQueryKey: ({ sessionId }) => getAttendanceQueryKey(sessionId),
-    optimisticUpdate: ({ sessionId, player, changes }) => {
-      const queryKey = getAttendanceQueryKey(sessionId);
-      patchItem(player.id, queryKey, changes);
-    },
+  const updateMutation = useOptimisticMutation({
+    mutationFn: upsertAttendanceAction,
+    getQueryKey: getAttendanceQueryKey,
+    optimisticUpdate: updateItem,
   });
 
   return {
     registerPlayer(sessionId: string, player: Player) {
-      registerMutation.mutate({ sessionId, player });
+      registerMutation.mutate({
+        session_id: sessionId,
+        player_id: player.id,
+        registered: true,
+        locked: false,
+      });
     },
 
     deregisterPlayer(sessionId: string, player: Player) {
       updateMutation.mutate({
-        sessionId,
-        player,
-        changes: {
-          registered: false,
-        },
+        session_id: sessionId,
+        player_id: player.id,
+        registered: false,
+        locked: false,
       });
     },
 
     lockPlayer(sessionId: string, player: Player) {
       updateMutation.mutate({
-        sessionId,
-        player,
-        changes: {
-          locked: true,
-        },
+        session_id: sessionId,
+        player_id: player.id,
+        registered: true,
+        locked: true,
       });
     },
 
     unlockPlayer(sessionId: string, player: Player) {
       updateMutation.mutate({
-        sessionId,
-        player,
-        changes: {
-          locked: false,
-        },
+        session_id: sessionId,
+        player_id: player.id,
+        registered: true,
+        locked: false,
       });
     },
   };
