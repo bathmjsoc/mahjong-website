705830f7da0154981e99e9fbbedc7e0c14d98ef3
Edwin <ea994@bath.ac.uk>
2026-09-13 23:53:07 +0900

    refactor: Minor cleanup of `useAttendance` + uses

--- src/hooks/attendance/useAttendance.ts
+++ src/hooks/attendance/useAttendance.ts
@@ -4,7 +4,6 @@ import type { Attendance } from "@/lib/types";
 import { useSessionContext } from "@/providers/SessionProvider";
 
 type UseAttendanceType = {
-  attendance: Attendance[];
   lockedPlayerIds: Set<string>;
   registeredPlayerIds: Set<string>;
 };
@@ -35,7 +34,7 @@ function selectAttendance(attendance: Attendance[]): UseAttendanceType {
     }
   }
 
-  return { attendance, lockedPlayerIds, registeredPlayerIds };
+  return { lockedPlayerIds, registeredPlayerIds };
 }
 
 async function fetchAttendance(sessionId: string): Promise<Attendance[]> {
